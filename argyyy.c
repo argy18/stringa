a700c4a296e26cce44d490dfc39daf91eb3c7f41
argy18 <127745460+argy18@users.noreply.github.com>
2023-03-16 14:34:35 +0100

    Add files via upload

---
+++ argyyy.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main () {
+    char stringa[] = "";
+
+    printf("scrivi una parola: ");
+    scanf(" %s", stringa);
+   
+    int i = 0;
+    while(stringa[i] != '\0') 
+    {
+        i++;
+    }
+   
+    while (i >= 0)
+    {
+      printf("%c", stringa[i]);
+      i--;
+      }
+}
+ 
